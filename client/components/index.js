f3ad98753f489b8eede4070114087fdf3148a10b
Jasiu Leja <jleja09@gmail.com>
2017-09-15 19:54:28 -0400

    adds navbar to Main component

--- client/components/index.js
+++ client/components/index.js
@@ -1,4 +1,5 @@
 export {default as Main} from './Main';
 export {BasicInput, BasicInputWithCheckbox, BasicTextArea} from './BasicInput';
 export {default as Creator} from './Creator';
+export {Login, Signup} from './auth-form';
 export {default as Preview} from './Preview';
