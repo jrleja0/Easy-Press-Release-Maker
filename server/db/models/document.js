4e18ed970420f9c3a23d4a19a7af17cbc998c8fe
Jasiu Leja <jleja09@gmail.com>
2017-09-22 17:16:04 -0400

    fixes mainTextSnippet fn getter method

--- server/db/models/document.js
+++ server/db/models/document.js
@@ -1,51 +1,43 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Document = db.define('document', {
-  crop: {
-    type: Sequelize.BOOLEAN
+const Document = db.define('document',
+  {
+    crop: {
+      type: Sequelize.BOOLEAN
+    },
+    imgSrc: {
+      type: Sequelize.STRING
+    },
+    Title: {
+      type: Sequelize.STRING
+    },
+    Date: {
+      type: Sequelize.STRING
+    },
+    Location: {
+      type: Sequelize.STRING
+    },
+    'Main Text': {
+      type: Sequelize.TEXT
+    },
+    'Secondary Text': {
+      type: Sequelize.TEXT
+    },
+    'Text About Company': {
+      type: Sequelize.TEXT
+    },
+    'Image Credits': {
+      type: Sequelize.STRING
+    }
   },
-  imgSrc: {
-    type: Sequelize.STRING
-  },
-  Title: {
-    type: Sequelize.STRING
-  },
-  Date: {
-    type: Sequelize.STRING
-  },
-  Location: {
-    type: Sequelize.STRING
-  },
-  'Main Text': {
-    type: Sequelize.TEXT
-  },
-  'Secondary Text': {
-    type: Sequelize.TEXT
-  },
-  'Text About Company': {
-    type: Sequelize.TEXT
-  },
-  'Image Credits': {
-    type: Sequelize.STRING
+  {
+    getterMethods: {
+      mainTextSnippet: function() {
+        return this['Main Text'].slice(0, 300) + '...';
+      }
+    }
   }
-});
+);
 
 module.exports = Document;
-
-/*///
- InstanceMethods
-*////
-// Document.prototype.mainTextSnippet = () => {
-//   return this['Main Text'].slice(0, 50) + '...';
-// };
-
-/*///
- Hooks
-*////
-const mainTextSnipper = doc => {
-  doc.mainTextSnippet = doc['Main Text'].slice(0, 50) + '...';
-};
-
-Document.beforeCreate(mainTextSnipper);
-Document.beforeUpdate(mainTextSnipper);
