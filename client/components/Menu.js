ff7afaf46f95928db4e0e96e3b9d12b88bf8f8ff
Jasiu Leja <jleja09@gmail.com>
2017-11-07 19:47:08 -0500

    adds formatting to Main headers, updates main Menu buttons, and adds grid UI to Menu without Bootstrap

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -10,25 +10,32 @@ import { fetchNewDoc, fetchUserDocs, fetchDoc} from '../store';
 const Menu = ({ userDocs, handleCreateNewDoc, handleFetchUserDocs, handleFetchDoc }) => {
   return (
     <div>
-      <div>
-        <a role="button" className="btn btn-success" tabIndex="0" onClick={handleCreateNewDoc}>Create New Doc</a>
+      <div className="div-main-menu-buttons">
+        <a role="button" className={'btn btn-success' + (userDocs && userDocs.length ? ' btn-small' : '')} tabIndex="0" onClick={handleCreateNewDoc}>Create New Doc</a>
       </div>
-      <div>
+      <div className="div-main-menu-buttons" style={{display: userDocs && userDocs.length ? 'none' : 'initial'}}>
         <a role="button" className="btn btn-primary" tabIndex="0" onClick={handleFetchUserDocs}>Open Saved Doc</a>
       </div>
-      <div>
+      <div className="container-fluid">
       { userDocs && userDocs.length ?
-          userDocs.map(doc => (
+          userDocs.map((doc, i) => (
             <div key={doc.Title}>
-              <a role="button" tabIndex="0" onClick={() => handleFetchDoc(doc.id)}>
-                <h3>{doc.Title}</h3>
-              </a>
-              { doc.imgSrc &&
-              <img className="img-fluid menu-img-thumbnail" src={doc.imgSrc} />
+              <div className="col-sm-12 col-md-6 col-lg-4">
+                <a role="button" tabIndex="0" onClick={() => handleFetchDoc(doc.id)}>
+                  <h3>{doc.Title}</h3>
+                </a>
+                { doc.imgSrc ?
+                  <img className="img-fluid menu-img-thumbnail" src={doc.imgSrc} />
+                  : <div className="img-fluid menu-img-thumbnail no-thumbnail-fill" />
+                }
+                <p>{doc.Date}</p>
+                <p>{doc.Location}</p>
+                <p>{doc.mainTextSnippet}</p>
+              </div>
+              { (i + 1) % 3 === 0 ?
+                <div className="lg-clearfix" />
+                : null
               }
-              <p>{doc.Date}</p>
-              <p>{doc.Location}</p>
-              <p>{doc.mainTextSnippet}</p>
             </div>
           ))
         : null
